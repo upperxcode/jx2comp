369454e3da62d92c73a96b29698ac12a43192683
Johnalb <joaoalbuquerque.br@gmail.com>
2025-08-22 23:34:38 -0300

    componente pai

--- lib/jx2comp.dart
+++ lib/jx2comp.dart
@@ -1,1 +1,8 @@
 export 'package:jx2_core/jx2_core.dart';
+export 'package:jx_data/jx_data.dart';
+//export 'package:jx_route/jx_route.dart';
+export 'package:jx_utils/jx_utils.dart';
+export 'package:jx2_grid/jx2_grid.dart';
+//export 'package:jx2_http_client/jx2_http_client.dart';
+export 'package:jx2_platform/jx2_platform.dart';
+export 'package:jx2_widgets/jx2_widgets.dart';
