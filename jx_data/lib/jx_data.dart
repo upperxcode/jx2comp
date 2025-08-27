baeb75d52bcd56af46843203e1cd25ae15c5f3b5
Johnalb <joaoalbuquerque.br@gmail.com>
2025-08-27 08:32:18 -0300

    componente pai

--- jx_data/lib/jx_data.dart
+++ jx_data/lib/jx_data.dart
@@ -2,3 +2,5 @@ export 'components/stores/base_store.dart';
 export 'components/widgets/dropdown/jx_search_dropdown.dart';
 export 'components/widgets/jx_field_form.dart';
 export 'components/widgets/row_flex.dart';
+export 'components/models/jx_field.dart';
+export 'components/models/jx_model.dart';
--- lib/jx_data.dart
+++ lib/jx_data.dart
@@ -1,2 +1,1 @@
-export 'package:jx_data/components/stores/base_store.dart';
-export 'package:jx_data/components/models/jx_field.dart';
+export 'package:jx_data/jx_data.dart';
